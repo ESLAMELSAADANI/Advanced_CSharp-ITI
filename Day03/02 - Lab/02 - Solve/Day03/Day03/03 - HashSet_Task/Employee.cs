0c6ec7877791128ab11c078f7efca540a1f6d694
Eslam Ashraf <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-09-20 17:07:56 +0300

    Implement IEquatable in Employee class
    Updated the Employee class to implement the IEquatable<Employee> interface. Added Equals(Employee? other) for null checking and Id comparison, and modified Equals(object? obj) to utilize it. Implemented GetHashCode() to return the Id's hash code. Removed commented-out code for clarity.

--- Day03/02 - Lab/02 - Solve/Day03/Day03/03 - HashSet_Task/Employee.cs
+++ Day03/02 - Lab/02 - Solve/Day03/Day03/03 - HashSet_Task/Employee.cs
@@ -6,28 +6,28 @@ using System.Threading.Tasks;
 
 namespace Day03._03___HashSet_Task
 {
-    internal class Employee //: /*IEquatable<Employee>*/
+    internal class Employee : IEquatable<Employee>
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Salary { get; set; }
 
-        //public bool Equals(Employee? other)
-        //{
-        //    if (other is null)
-        //        return false;
-        //    return Id == other.Id;
-        //}
+        public bool Equals(Employee? other)
+        {
+            if (other is null)
+                return false;
+            return Id == other.Id;
+        }
 
-        //public override bool Equals(object? obj)
-        //{
-        //    return Equals(obj as Employee);
-        //}
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Employee);
+        }
 
-        //public override int GetHashCode()
-        //{
-        //    return Id.GetHashCode();
-        //}
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
 
         public override string ToString()
         {
