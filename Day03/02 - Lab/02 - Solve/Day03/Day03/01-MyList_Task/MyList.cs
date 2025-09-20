6bd2a77a794f2f13911319fd5d69a1de463b56dd
Eslam Ashraf <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-09-20 14:44:41 +0300

    01-MyList_Task

---
+++ Day03/02 - Lab/02 - Solve/Day03/Day03/01-MyList_Task/MyList.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day03._01_MyList_Task
+{
+    internal class MyList<T> where T : IComparable<T>
+    {
+        //Fields
+        T[] arr;
+        int capacity;
+        int top;
+
+        //Properties
+        public int Capacity { get => capacity; }
+        public int length { get => top + 1; }
+
+        //Indexer
+        public T this[int index]
+        {
+            get
+            {
+                if (top != -1 && index <= top)
+                {
+                    return arr[index];
+                }
+                else
+                {
+                    Console.WriteLine("There is no Data!");
+                    throw new ArgumentOutOfRangeException();
+                }
+
+            }
+            set
+            {
+                if (top != -1 && index <= top)
+                {
+                    arr[index] = value;
+                }
+                else
+                {
+                    Console.WriteLine("Can't Set New Data - Just Update!");
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+
+
+        //Constructor
+        public MyList(int cap)
+        {
+            capacity = cap;
+            arr = new T[cap];
+            top = -1;
+        }
+
+        //Methods
+        public void Add(T value)
+        {
+            if (top < capacity - 1)
+            {
+                top++;
+                arr[top] = value;
+            }
+            else
+            {
+                capacity *= 2;
+                T[] temp = new T[capacity];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    temp[i] = arr[i];
+                }
+                arr = temp;
+                top++;
+                arr[top] = value;
+                //throw new IndexOutOfRangeException();
+            }
+
+        }
+
+        public void Remove(T value)
+        {
+            if (length == 0)
+            {
+                Console.WriteLine("Array Is Empty!");
+                return;
+            }
+            bool valueFound = false;
+            for (int i = 0; i < top; i++)
+            {
+                if (value.CompareTo(arr[i]) == 0)
+                {
+                    valueFound = true;
+                    arr[i] = arr[i + 1];
+                    for (int j = i + 1; j < top; j++)
+                    {
+                        arr[j] = arr[j + 1];
+                    }
+                    capacity--;
+                    top--;
+                    T[] newArr = new T[capacity];
+                    for (int k = 0; k <= top; k++)
+                    {
+                        newArr[k] = arr[k];
+                    }
+                    arr = newArr;
+                    break;
+                }
+            }
+            if (!valueFound)
+                Console.WriteLine($"Not Found This Value -> {value}!");
+        }
+
+        public void Print()
+        {
+            foreach (var item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
---
+++ Day03/02 - Lab/02 - Solve/Day03/Day03/Program.cs
@@ -0,0 +1,72 @@
+﻿using Day03._01_MyList_Task;
+
+namespace Day03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> l1 = new MyList<int>(5);
+
+            Console.WriteLine($"Capacity: {l1.Capacity}");//5
+            Console.WriteLine($"Length: {l1.length}");//0
+            Console.WriteLine("=============");
+
+            l1.Add(1);
+            l1.Add(2);
+            l1.Add(3);
+            l1.Add(4);
+            l1.Add(5);
+
+            l1.Print();
+            Console.WriteLine($"\nCapacity: {l1.Capacity}");//5
+            Console.WriteLine($"Length: {l1.length}");//5
+            Console.WriteLine("=============");
+
+            l1.Add(6);
+
+            l1.Print();
+            Console.WriteLine($"\nCapacity: {l1.Capacity}");//10
+            Console.WriteLine($"Length: {l1.length}");//6
+            Console.WriteLine("=============");
+
+            l1.Add(7);
+            l1.Add(8);
+            l1.Add(9);
+            l1.Add(10);
+
+            l1.Print();
+            Console.WriteLine($"\nCapacity: {l1.Capacity}");//10
+            Console.WriteLine($"Length: {l1.length}");//10
+            Console.WriteLine("=============");
+
+
+            l1.Remove(8);
+            l1.Remove(4);
+            l1.Remove(25);//Not Found This Value -> 25!
+            l1.Print();//1 2 3 5 6 7 9 10
+            Console.WriteLine($"\nCapacity: {l1.Capacity}");//Capacity: 8
+            Console.WriteLine($"Length: {l1.length}");//Length: 8
+            Console.WriteLine("=============");
+
+            l1.Add(7);
+            l1.Add(8);
+            l1.Print();//1 2 3 5 6 7 9 10 7 8 0 0 0 0 0 0
+            Console.WriteLine($"\nCapacity: {l1.Capacity}");//Capacity: 16
+            Console.WriteLine($"Length: {l1.length}");//Length: 10
+            Console.WriteLine("=============");
+
+            Console.WriteLine(l1[0]);//1
+            Console.WriteLine(l1[9]);//8
+            //Console.WriteLine(l1[11]);//Unhandled exception. System.ArgumentOutOfRangeException: Specified argument was out of the range of valid values.
+            Console.WriteLine("=============");
+
+            l1[0] = 20;
+            l1[9] = 40;
+            //l1[15] = 25;//Unhandled exception. System.ArgumentOutOfRangeException: Specified argument was out of the range of valid values.
+
+            l1.Print();//20 2 3 5 6 7 9 10 7 40 0 0 0 0 0 0
+
+        }
+    }
+}
